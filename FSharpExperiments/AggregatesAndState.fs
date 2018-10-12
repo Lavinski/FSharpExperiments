4a55c0e1052336cdceed16ee0d9020363674a7fd
Daniel Little <lavinski@live.com.au>
2018-10-12 17:17:31 +1000

    Add some experiments

---
+++ FSharpExperiments/AggregatesAndState.fs
@@ -0,0 +1,104 @@
+﻿module AggregatesAndState
+
+let o = obj()
+
+type AggregateCommand =
+    | Create of obj
+    | Update of obj
+    | Delete of obj
+    //| Restore of obj
+
+type AggregateEvent =
+    | Created of obj
+    | Updated of obj
+    | Deleted of obj
+
+[<RequireQualifiedAccess>]
+type State =
+    | Identity
+    | Created
+    | Updated
+    | Deleted
+
+// behaviour
+let create payload state =
+    Created payload
+// apply
+let created event state =
+    State.Created
+
+
+let update payload state =
+    Updated payload
+
+let updated event state =
+    State.Updated
+
+
+let delete payload state =
+    Deleted payload
+
+let deleted event state =
+    State.Deleted
+
+
+let handle2 command state =
+    match state with
+    | State.Identity ->
+        match command with
+        | Create c -> create state c
+        | _ -> failwithf "Command %A could not be handled in state %A" command state
+    | State.Created ->
+        match command with
+        | Update c -> update state c
+        | _ -> failwithf "Command %A could not be handled in state %A" command state
+    | State.Updated ->
+        match command with
+        | Delete c -> delete state c
+        | _ -> failwithf "Command %A could not be handled in state %A" command state
+    | State.Deleted ->
+        failwithf "Command %A could not be handled in state %A" command state
+
+let handle command state =
+    match state, command with
+    | State.Identity, Create payload -> create payload state
+    | State.Created, Update payload -> update payload state
+    | State.Updated, Delete payload -> delete payload state
+    | _, Create _
+    | _, Update _
+    | _, Delete _ ->
+        failwithf "Command %A could not be handled in state %A" command state
+
+let apply state event =
+    match event with
+    | Created c -> created state c
+    | Updated c -> updated state c
+    | Deleted c -> deleted state c
+
+let run () =
+    let flip f x y = f y x
+
+    let finalState =
+        let apply = flip apply
+        State.Identity
+            |> apply (Created o)
+            |> apply (Updated o)
+            |> apply (Deleted o)
+
+    printfn "Final State after apply: %A" finalState
+
+    let happen command state =
+        handle command state |> apply state
+
+    let finalState =
+        State.Identity
+            |> happen (Create o)
+            |> happen (Update o)
+            |> happen (Delete o)
+            |> happen (Delete o)
+
+    printfn "Final State after handle: %A" finalState
+
+
+//Apply(this, event)
+
---
+++ FSharpExperiments/EventedSagasAndStateMachines.fs
@@ -0,0 +1,75 @@
+﻿module EventedSagasAndStateMachines
+
+open System
+
+// ExternalEvent State -> InternalEvent Command
+
+type ExternalEvents =
+    | CompanyRegistrationReceived
+    | RegistrationApproved
+    | CompanyEnrolled
+
+type ExternalCommands =
+    | EnrollCompany
+
+type ProcessEvents =
+    | ReceivedRegistration
+    | ReceivedApproval
+    | Enrolled
+
+type ProcessState =
+    | WaitingForRegistration
+    | WaitingForApproval
+    | WaitingForEnrollment
+    | Completed
+
+let reactTo state externalEvent =
+    match state, externalEvent with
+    | WaitingForRegistration, CompanyRegistrationReceived -> [ReceivedRegistration], []
+    | WaitingForApproval, RegistrationApproved -> [ReceivedApproval], [EnrollCompany]
+    | WaitingForEnrollment, CompanyEnrolled -> [Enrolled], []
+    | _ -> failwithf "Event %A could not be handled in state %A" externalEvent state
+
+let apply state event =
+    match event with
+    | ReceivedRegistration -> WaitingForApproval
+    | ReceivedApproval -> WaitingForEnrollment
+    | Enrolled -> Completed
+
+let flip f x y = f y x
+
+let hydrate events =
+    List.fold apply WaitingForRegistration events
+
+let execute events externalEvent =
+    hydrate events |> (flip reactTo) externalEvent
+
+
+type State2 =
+    | First
+    | Second
+
+type ExternalOne =
+    | EventA of Guid
+
+type ExternalTwo =
+    | EventB of Guid
+
+type ExternalEvent =
+    | ExternalOneEventA
+    | ExternalTwoEventB
+    | Ignore
+
+let conform (externalEvent: obj) = 
+    match externalEvent with
+    | (:? ExternalOne as x) when (match x with | EventA id -> true | _ -> false) -> (id, ExternalOneEventA) |> Some
+    | (:? ExternalTwo as x) when (match x with | EventB id -> true | _ -> false) -> (id, ExternalTwoEventB) |> Some
+    | _ -> None
+
+//let external = conform externalEvent
+
+let reactTo2 (external: ExternalEvent) (state: State2) =
+    match state, external with
+    | First, ExternalOneEventA -> [ReceivedRegistration], []
+    | Second, ExternalTwoEventB -> [ReceivedApproval], [EnrollCompany]
+    | _ -> failwithf "Event %A could not be handled in state %A" external state
